486d0711c04770e7e1c82d1c5bd0e1bd3b6ba7ee
chikchicly <chikchicly@gmail.com>
2025-03-22 09:24:27 +0700

    news commits

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views','public');
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
-  response.send('Hello World!')
+  response.render("index",{
+    title: 'oK',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, harum id? Recusandae facere, dolor quos voluptas voluptatibus ipsa! Officiis provident deserunt ad culpa ducimus eius earum consequuntur praesentium ipsum qui?'
+  });
 })
 
 app.listen(port, () => {
